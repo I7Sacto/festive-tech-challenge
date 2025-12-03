0dd0e8f9dd400ecfd5eeb790d1160371575b3685
Anna Vorobey <annavorobey393@gmail.com>
2025-12-03 16:28:39 +0200

    Add empty Puzzle component

---
+++ src/components/Puzzle.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Puzzle: React.FC = () => {
+    return (
+        <div>
+            {/* Empty Puzzle Component */}
+        </div>
+    );
+};
+
+export default Puzzle;
