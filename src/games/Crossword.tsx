0e9b1a3a9fb4c3e9ddb980f73058360f936f65c0
Anna Vorobey <annavorobey393@gmail.com>
2025-12-03 16:26:51 +0200

    Add Crossword component

---
+++ src/games/Crossword.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+const Crossword = () => (
+  <div style={{padding:"2rem",textAlign:"center"}}>
+    <h1>Технічний кросворд</h1>
+    <p>Гра буде доступна після проходження вікторини на 70 балів.</p>
+  </div>
+);
+export default Crossword;
