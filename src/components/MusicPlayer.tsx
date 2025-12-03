cfca73e953e243c4e65011722f6d48ea91fb462c
Anna Vorobey <annavorobey393@gmail.com>
2025-12-03 16:25:40 +0200

    Додаю пусті компоненти для ігор, MusicPlayer, шаблон для вікторини, та блокування кросворду

---
+++ src/components/MusicPlayer.tsx
@@ -0,0 +1,3 @@
+import React from "react";
+const MusicPlayer = () => (<audio controls autoPlay loop><source src="/music/christmas.mp3" type="audio/mpeg" />Ваш браузер не підтримує елемент аудіо.</audio>);
+export default MusicPlayer;
