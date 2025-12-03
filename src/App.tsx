5b24e6d115bd6aea1f3f0ae390261334d84a2c26
Anna Vorobey <annavorobey393@gmail.com>
2025-12-03 17:15:40 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "@/contexts/AuthContext";
+
+// Pages
 import Index from "./pages/Index";
 import Games from "./pages/Games";
 import Stories from "./pages/Stories";
@@ -10,8 +13,11 @@ import Music from "./pages/Music";
 import Gifts from "./pages/Gifts";
 import Gallery from "./pages/Gallery";
 import NotFound from "./pages/NotFound";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
 
-// Import all games
+// Games
 import Quiz from "./pages/games/Quiz";
 import Crossword from "./pages/games/Crossword";
 import Puzzle from "./pages/games/Puzzle";
@@ -27,26 +33,33 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <Routes>
-          {/* Main pages */}
-          <Route path="/" element={<Index />} />
-          <Route path="/games" element={<Games />} />
-          <Route path="/stories" element={<Stories />} />
-          <Route path="/music" element={<Music />} />
-          <Route path="/gifts" element={<Gifts />} />
-          <Route path="/gallery" element={<Gallery />} />
-          
-          {/* Game routes */}
-          <Route path="/games/quiz" element={<Quiz />} />
-          <Route path="/games/crossword" element={<Crossword />} />
-          <Route path="/games/puzzle" element={<Puzzle />} />
-          <Route path="/games/coding" element={<Coding />} />
-          <Route path="/games/networking" element={<Networking />} />
-          <Route path="/games/surprise" element={<Surprise />} />
-          
-          {/* 404 page */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <AuthProvider>
+          <Routes>
+            {/* Main pages */}
+            <Route path="/" element={<Index />} />
+            <Route path="/games" element={<Games />} />
+            <Route path="/stories" element={<Stories />} />
+            <Route path="/music" element={<Music />} />
+            <Route path="/gifts" element={<Gifts />} />
+            <Route path="/gallery" element={<Gallery />} />
+            
+            {/* Auth pages */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            
+            {/* Game routes */}
+            <Route path="/games/quiz" element={<Quiz />} />
+            <Route path="/games/crossword" element={<Crossword />} />
+            <Route path="/games/puzzle" element={<Puzzle />} />
+            <Route path="/games/coding" element={<Coding />} />
+            <Route path="/games/networking" element={<Networking />} />
+            <Route path="/games/surprise" element={<Surprise />} />
+            
+            {/* 404 page */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </AuthProvider>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
