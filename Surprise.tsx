006fb509913a716ba4f5d3b71a6c7003c5cfc8f5
Anna Vorobey <annavorobey393@gmail.com>
2025-12-03 16:28:46 +0200

    Add empty Surprise.tsx component

---
+++ Surprise.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Surprise: React.FC = () => {
+    return (
+        <div>
+            {/* Your surprise content here */}
+        </div>
+    );
+};
+
+export default Surprise;
